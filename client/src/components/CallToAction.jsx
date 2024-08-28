215703da8adf828ade5abd3eb9fda72d40f540d1
abdillahi <hajiabdi213@gmail.com>
2024-08-28 21:19:36 +0300

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,33 @@
+import { Button } from "flowbite-react";
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <p className="text-gray-500 my-2">
+          Checkout these resources with 100 JavaScript Projects
+        </p>
+        <Button
+          className="rounded-tl-xl rounded-bl-none"
+          gradientDuoTone="purpleToPink"
+        >
+          {" "}
+          <a
+            href="https://korodhsoaqoon.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            100 JavaScript Projects
+          </a>
+        </Button>
+      </div>
+
+      <div className="p-7 flex-1">
+        <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20221114110410/Top-10-JavaScript-Project-Ideas-For-Beginners-2023.png" />
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
